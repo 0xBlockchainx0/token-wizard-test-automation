42b7888ef47d1c9476aa0a6dcd103d662d3bc15d
person <person@persons-MacBook-Pro.local>
2018-04-02 11:02:17 -0700

    path to CSV file

--- pages/WizardStep3.js
+++ pages/WizardStep3.js
@@ -210,8 +210,8 @@ catch(err)
 
 	     const loc = By.xpath("//*[@id=\"root\"]/div/section/div[3]/div/div[2]/div[2]/div[2]/div/input");
 	     var el = this.driver.findElement(loc);
-	     el.sendKeys("/home/travis/build/dennis00010011b/travistest/node_modules/token-wizard-test-automation/MyWhitelist.csv");
-	     // el.sendKeys("./node_modules/token-wizard-test-automation/MyWhitelist.csv");
+	     //el.sendKeys("/home/travis/build/dennis00010011b/travistest/node_modules/token-wizard-test-automation/MyWhitelist.csv");
+	     el.sendKeys("../node_modules/token-wizard-test-automation/MyWhitelist.csv");
 	     //el.sendKeys("/Users/person/WebstormProjects/token-wizard-test-automation/MyWhitelist.csv")
 	     //await super.clickWithWait(buttonUploadCSV);
 	     return true;
