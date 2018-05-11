53223fed1ac8f78da083e28d8456dee313ede1fa
dennis00010011b <person@persons-MacBook-Pro.local>
2018-05-10 22:00:28 -0700

    refactoring

--- pages/CrowdsalePage.js
+++ pages/CrowdsalePage.js
@@ -1,25 +1,28 @@
-const logger=require('../entity/Logger.js').logger;
-const Page=require('./Page.js').Page;
-const By=require('selenium-webdriver/lib/by').By;
-const buttonInvest=By.className("button button_fill");
+const logger = require('../entity/Logger.js').logger;
+const Page = require('./Page.js').Page;
+const By = require('selenium-webdriver/lib/by').By;
+const buttonInvest = By.className("button button_fill");
 
 class CrowdsalePage extends Page {
 
-    constructor(driver){
-        super(driver);
-        this.URL;
-        this.name="Crowdsale page :";
-    }
-   async isDisplayedButtonInvest() {
-	   logger.info(this.name+" isDisplayedButtonInvest ");
-	   return super.isElementDisplayed(buttonInvest);
-   }
-   async  clickButtonInvest() {
-        logger.info(this.name+"clickButtonInvest ");
-        return await super.clickWithWait(buttonInvest);
-   }
+	constructor(driver) {
+		super(driver);
+		this.URL;
+		this.name = "Crowdsale page :";
+	}
+
+	async isDisplayedButtonInvest() {
+		logger.info(this.name + " isDisplayedButtonInvest ");
+		return super.isElementDisplayed(buttonInvest);
+	}
+
+	async clickButtonInvest() {
+		logger.info(this.name + "clickButtonInvest ");
+		return await super.clickWithWait(buttonInvest);
+	}
 
 }
-module.exports={
-    CrowdsalePage:CrowdsalePage
+
+module.exports = {
+	CrowdsalePage: CrowdsalePage
 }
